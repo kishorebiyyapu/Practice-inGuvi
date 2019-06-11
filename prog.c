8abc33ddb53bcf733efaed7fdc10ba3dad7719a3
kishorebiyyapu <36230007+kishorebiyyapu@users.noreply.github.com>
2019-06-11 14:48:12 +0530

    Create prog.c

---
+++ prog.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n>0)
+{
+if(n%2==0)
+{
+printf("Even");
+}
+else
+{
+printf("Odd");
+}
+}
+else
+{
+printf("Invalid");
+}
+return 0;
+}
