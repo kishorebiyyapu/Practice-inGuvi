11b6463a04231652f16dd729517382a2fb1b4858
kishorebiyyapu <36230007+kishorebiyyapu@users.noreply.github.com>
2019-06-16 11:19:39 +0530

    Create prog6.c

---
+++ prog6.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+ int a;
+ scanf("%d",&a);
+ if(a%4==0)
+ {
+    if(a%100==0)
+	{
+	   if(a%400==0)
+	   {
+	   	printf("yes");
+	   }	
+	   else
+	   {
+	   	printf("no");
+	   }
+	}
+	printf("yes");	
+  }
+  else  
+     printf("no");
+return 0;
+}
