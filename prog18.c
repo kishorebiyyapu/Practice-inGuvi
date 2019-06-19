e3c31ec1db0d3225135287e30456e99afc891cf1
kishorebiyyapu <36230007+kishorebiyyapu@users.noreply.github.com>
2019-06-19 19:12:04 +0530

    Update prog18.c

--- prog18.c
+++ prog18.c
@@ -1,1 +1,24 @@
-
+#include<stdio.h>
+int main()
+{
+	int a,b,c,s,i,rem;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	for(i=a;i<=b;i++)
+	{
+		s=0;
+		c=i;
+		while(c!=0)
+		{
+			rem=c%10;
+			s=s+(rem*rem*rem);
+			c=c/10;
+		}
+		if(i==s)
+		{
+		   printf("%d ",i);
+	    }
+	
+	}
+	return 0;
+}
